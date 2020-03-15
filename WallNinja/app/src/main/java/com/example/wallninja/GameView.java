86dad9a68662a5b95e195c8f297d99bccbf2d060
mihej18 <mikkel@MikkelsPC.localdomain>
2020-03-15 16:15:47 +0100

    Added physics

--- WallNinja/app/src/main/java/com/example/wallninja/GameView.java
+++ WallNinja/app/src/main/java/com/example/wallninja/GameView.java
@@ -9,6 +9,7 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Handler;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class GameView extends View {
@@ -22,9 +23,12 @@ public class GameView extends View {
     Point point;
     int dWith, dHeight; // Device width and height respectively
     Rect rect;
-
     // Creating a bitmap array for Ninja:
     Bitmap[] ninjas;
+    // We need an integet variable to keep track of the ninja image/frame
+    int ninjaFrame = 0;
+    int velocity=0,gravity=3; //
+    int ninjaX, ninjaY;
 
 
     public GameView(Context context) {
@@ -45,7 +49,11 @@ public class GameView extends View {
         rect = new Rect(0,0,dWith,dHeight);
 
         ninjas = new Bitmap[2];
-        ninjas[0] = BitmapFactory.decodeByteArray(get)
+        ninjas[0] = BitmapFactory.decodeResource(getResources(),R.drawable.ninja);
+        ninjas[1] = BitmapFactory.decodeResource(getResources(),R.drawable.ninja2);
+        ninjaX = dWith/2 - ninjas[0].getWidth()/2; //Initally ninja will be in centre
+        ninjaY = dHeight/2 - ninjas[0].getHeight()/2;
+
     }
 
     @Override
@@ -55,6 +63,33 @@ public class GameView extends View {
         // Draw the background on canvas
         //canvas.drawBitmap(background,0,0,null);
         canvas.drawBitmap(background,null,rect ,null); // fixed background
+        if(ninjaFrame==0){
+            ninjaFrame = 1;
+        }else{
+            ninjaFrame = 0;
+        }
+
+        velocity += gravity;
+        ninjaY += velocity;
+
+        // This will place the ninja on the center of the screen.
+        // Both ninjas[0] and ninjas[1] have same dimension.
+        canvas.drawBitmap(ninjas[ninjaFrame],dWith/2 - ninjaX, ninjaY,null);
         handler.postDelayed(runnable,UPDATE_MILLIS);
+    }
+    //Get touch event
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int action = event.getAction();
+        if(action == MotionEvent.ACTION_DOWN){ //this is tap is detected on screen
+            //Here we want the bird to move upwards by some unit
+            velocity = - 30; // 30 units on onward direction
+
+        }
+
+        return true; //By returning true in indicates that we've done with touch event and no further action is required by Android
     }
 }
